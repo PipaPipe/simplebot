44645cd6da9db3915afc0844c0881ee2594bbda2
Maksim Sadkov <sy_sy_sy_sy@mail.ru>
2021-07-26 13:39:46 +0500

    simplebot v2

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
+import ephem
+import datetime
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 					level=logging.INFO,
@@ -20,11 +22,36 @@ def chat(update, context):
 	update.message.reply_text(text)
 
 
+def send_planet(update, context):
+
+	planet_input = (update.message.text.split()[1]).lower().capitalize()
+	print(planet_input)
+
+	today = datetime.datetime.today()
+	format_today = today.strftime("%Y/%m/%d")
+
+	planets = {"Mercury": ephem.Mercury(format_today), 
+    "Venus": ephem.Venus(format_today), 
+    "Mars" : ephem.Mars(format_today), 
+    "Jupiter": ephem.Jupiter(format_today), 
+    "Saturn": ephem.Saturn(format_today), 
+    "Uranus": ephem.Uranus(format_today), 
+    "Neptune": ephem.Neptune(format_today)
+    }
+	if planet_input in planets.keys():
+		result_to_send = (ephem.constellation(planets[planet_input]))[1]
+		update.message.reply_text(result_to_send)
+	else:
+		result_to_send = "there is no planet {}".format(planet_input)
+		update.message.reply_text(result_to_send)
+    
 def main():
 	updtr = Updater(settings.TELEGRAM_API_KEY)
 
 	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+	updtr.dispatcher.add_handler(CommandHandler("planet", send_planet))
 	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
 	updtr.start_polling()
 	updtr.idle()
 
